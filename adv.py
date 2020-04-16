0b84d1d178e369aa998383157858105ebfcec893
Woober <ddelfaus@gmail.com>
2020-04-16 15:14:24 -0400

    initial commit

--- adv.py
+++ adv.py
@@ -1,3 +1,20 @@
+# There are a few smaller graphs in the file which you can test your traversal method on before committing to the large graph. You may find these easier to debug.
+
+# Start by writing an algorithm that picks a random unexplored direction from the player's current room,
+#  travels and logs that direction, then loops. 
+# This should cause your player to walk a depth-first traversal. When you reach a dead-end (i.e. a room with no unexplored paths)
+# , walk back to the nearest room that does contain an unexplored path.
+
+# You can find the path to the shortest unexplored room by using a breadth-first search for a room with a '?' for an exit.
+#  If you use the bfs code from the homework, you will need to make a few modifications.
+
+# Instead of searching for a target vertex, you are searching for an exit with a '?' as the value. 
+# If an exit has been explored, you can put it in your BFS queue like normal.
+
+# BFS will return the path as a list of room IDs. You will need to convert this to a list of n/s/e/w directions before you can add it to your traversal path.
+
+If all paths have been explored, you're done!
+
 from room import Room
 from player import Player
 from world import World
@@ -56,6 +73,7 @@ while True:
     cmds = input("-> ").lower().split(" ")
     if cmds[0] in ["n", "s", "e", "w"]:
         player.travel(cmds[0], True)
+       
     elif cmds[0] == "q":
         break
     else:
