90fe32101e3b1b9e6eb2222d0b3948209c3aaefc
Woober <ddelfaus@gmail.com>
2020-04-17 00:45:10 -0400

    updating plan and adding unexplored_rooms function

--- adv.py
+++ adv.py
@@ -12,6 +12,18 @@
 
 # If all paths have been explored, you're done!
 
+#plan
+
+#set up list/sets and things
+#store possible directions as question marks as key values
+#only move if question marks
+#randomly move and set question mark to room cords. to start
+#loop step 3 three until a dead end if found
+#use BFS to find a new question mark
+
+
+
+from util import Stack, Queue
 from room import Room
 from player import Player
 from world import World
@@ -25,10 +37,10 @@ world = World()
 
 # You may uncomment the smaller graphs for development and testing purposes.
 # map_file = "maps/test_line.txt"
-# map_file = "maps/test_cross.txt"
+map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
 # map_file = "maps/test_loop_fork.txt"
-map_file = "maps/main_maze.txt"
+# map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
 room_graph=literal_eval(open(map_file, "r").read())
@@ -43,31 +55,148 @@ player = Player(world.starting_room)
 # traversal_path = ['n', 'n']
 traversal_path = []
 visited_rooms = set()
+unexplored = []
 
 #added current room to visited
-visited_rooms.add(player.current_room)
+
+
+
+#store possible directions
+def unexplored_rooms(room_id): 
+ # set each direction to a ?
+    possible_directions = {}
+
+    if "n" in room_graph[room_id][1].keys():
+        possible_directions['n'] = '?'
+
+    if "e" in room_graph[room_id][1].keys():
+        possible_directions['e'] = '?'
+    if "s" in room_graph[room_id][1].keys():
+        possible_directions['s'] = '?'
+    if "w" in room_graph[room_id][1].keys():
+        possible_directions['w'] = '?'
+
+    return possible_directions
+    #broke code
+    # for path in player.current_room.get_exits():
+    #     unexplored.append("?")
+    #     print(unexplored)
+
+
+
+
+#need to add opposites when looking at next room, since when entering a room it should swap the opposite 
+def next_room_exits(next_id, next_direction):
+    
+    opposites = { 'n': 's', 's' : 'n', 'e': 'w', 'w': 'e'}
+    next_array = []
+    previous_arrray = []
+
+    for direction in room_graph[next_id][1].keys():
+
+        if direction == 'n':
+            next_array.append('s')
+
+        if direction == 's':
+            next_array.append('n')
+    
+        if direction == 'e':
+            next_array.append('w')
+    
+        if direction == 'w':
+            next_array.append('e')
+
+    for item in room_graph[next_id][1].keys():
+        previous_array.append(opposites[item])
+
+    if next_direction in next_array:
+
+        return True
+    else:
+        return False    
+        
+            
+
+ 
+
+
+
+#BFT to find the nearest unexplored exit with '?'
 
 # Start by writing an algorithm that picks a random unexplored direction from the player's current room
+
+
+
+def maze_pathfinding(start_room):
+    #id
+    room_id = (start_room)
+    #getting start directions
+    visited = { start_room : unexplored_rooms(start_room)}
+    room_choice_len = len(room_graph[start_room][1].keys())
+
+    current_room = start_room
+    #DFS type loop
+    while (len(visited_rooms) < len(world.rooms)):
+
+        #keeping running until out of ? or dead end
+        while '?' in visited[current_room].values():
+
+            # current room directions
+            room_directions =visited[current_room]
+            room_exits = []
+            index = 0
+            #setting room exits
+            for item in list(room_directions.values()):
+                if item == '?':
+                    room_exits.append(list(room_directions.keys())[index])
+
+                index +=1
+            #break loop if dead end
+            if len(room_directions) == 0:
+                print("Dead end")
+                break
+
+            #pick a random direction
+
+            new_direction = room_exits[random.randrange(len(room_exits))]
+            # print(new_direction)
+            next_room = room_graph[current_room][1][new_direction]
+            # print(next_room)  
+
+
+
+
+    
+      
 
 def random_direction():
     #get room exits
     exits = player.current_room.get_exits()
-    print(exits, "test")
+    visited_rooms.add(player.current_room)
+
+  
+
+    print(unexplored_rooms(player.current_room.id))
     #get random direction
     direction = exits[random.randint(0, len(exits) - 1)]
     #travel
     player.travel(direction)
     visited_rooms.add(player.current_room)
+    #  travels and logs that direction, 
     print(player.current_room.print_room_description(player))
-
+    # then loops. 
     while (len(visited_rooms) < len(world.rooms)):
+       
         exits = player.current_room.get_exits()
+        print(unexplored_rooms(player.current_room.id))
         direction = exits[random.randint(0, len(exits) - 1)]
+        print(direction, "this is a direction")
         player.travel(direction)
+        traversal_path.append(player.current_room)
         visited_rooms.add(player.current_room)
-        print(player.current_room.print_room_description(player))
+        print(player.current_room.print_room_description(player), "faewafeafaewfeaw")
 
-#  travels and logs that direction, then loops. 
+
 
 
 
@@ -105,7 +234,8 @@ def random_direction():
 
 player.current_room = world.starting_room
 visited_rooms.add(player.current_room)
-print(random_direction())
+# print(random_direction())
+maze_pathfinding(player.current_room.id)
 for move in traversal_path:
     player.travel(move)
     visited_rooms.add(player.current_room)
